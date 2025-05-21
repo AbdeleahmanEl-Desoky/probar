c37801b66f65e909d3edd4fc1dd082a61bde862c
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-05-22 02:11:39 +0300

    add

--- modules/Client/Shops/Services/ShopsFormatDistanceService.php
+++ modules/Client/Shops/Services/ShopsFormatDistanceService.php
@@ -11,29 +11,24 @@ class ShopsFormatDistanceService
 {
 
 
-    public function formatDistance(float $distanceInMeters): string
+    public static function formatDistance(float $distanceInMeters): string
     {
         $distanceInKm = $distanceInMeters / 1000;
 
         if ($distanceInKm < 1) {
-            // Less than 1 km, show in meters rounded to nearest 10 meters
-            $distanceInMetersRounded = round($distanceInMeters, -1); // e.g., 740 -> 740, 755 -> 760
-            return $distanceInMetersRounded . ' m';
+            return round($distanceInMeters, -1) . ' m';
         }
 
         if ($distanceInKm > 5) {
-            // If more than 5 km, cap at "5+ km"
             return '5+ km';
         }
 
-        // Between 1 and 5 km, round to 1 decimal place
         return round($distanceInKm, 1) . ' km';
     }
 
-    public  function calculateDistance(float $latFrom, float $lonFrom, float $latTo, float $lonTo): float
+    public static function calculateDistance(float $latFrom, float $lonFrom, float $latTo, float $lonTo): float
     {
-        $earthRadius = 6371000; // in meters
-
+        $earthRadius = 6371000; // meters
         $latFromRad = deg2rad($latFrom);
         $lonFromRad = deg2rad($lonFrom);
         $latToRad = deg2rad($latTo);
