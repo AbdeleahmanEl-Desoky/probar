b1d1ff231064cf7daf35e94ab2686e5a36d26efb
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-06-03 17:11:03 +0300

    shop

--- modules/Client/Shops/Resources/routes/api.php
+++ modules/Client/Shops/Resources/routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Client\Shops\Controllers\ShopsController;
 
-Route::group(['middleware' => ['auth:api_clients']], function () {
+// Route::group(['middleware' => ['auth:api_clients']], function () {
     Route::get('/', [ShopsController::class, 'index']);
-    Route::post('/', [ShopsController::class, 'store']);
+    // Route::post('/', [ShopsController::class, 'store']);
     Route::get('/{id}', [ShopsController::class, 'show']);
-    Route::put('/{id}', [ShopsController::class, 'update']);
-    Route::delete('/{id}', [ShopsController::class, 'delete']);
-});
+    // Route::put('/{id}', [ShopsController::class, 'update']);
+    // Route::delete('/{id}', [ShopsController::class, 'delete']);
+// });
