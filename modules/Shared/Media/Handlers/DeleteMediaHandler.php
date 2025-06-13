aa9678153b8d011fa15818f18ad6cf120437cc4d
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2025-06-14 02:30:41 +0300

    -

--- modules/Shared/Media/Handlers/DeleteMediaHandler.php
+++ modules/Shared/Media/Handlers/DeleteMediaHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Shared\Media\Handlers;
 
 use Modules\Shared\Media\Repositories\MediaRepository;
-use Ramsey\Uuid\UuidInterface;
 
 class DeleteMediaHandler
 {
