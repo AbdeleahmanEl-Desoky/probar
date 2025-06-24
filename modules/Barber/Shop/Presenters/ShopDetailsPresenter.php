348b4ac8110dd0060d4a52a7e70805d60357194d
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2025-06-25 01:21:38 +0300

    -

--- modules/Barber/Shop/Presenters/ShopDetailsPresenter.php
+++ modules/Barber/Shop/Presenters/ShopDetailsPresenter.php
@@ -9,6 +9,7 @@ use BasePackage\Shared\Presenters\AbstractPresenter;
 use Modules\Barber\ShopHour\Presenters\ShopHourPresenter;
 use Modules\Client\Rate\Presenters\RatePresenter;
 use Modules\Shared\Media\Presenters\MediaPresenter;
+use Carbon\Carbon;
 
 class ShopDetailsPresenter extends AbstractPresenter
 {
@@ -21,6 +22,29 @@ class ShopDetailsPresenter extends AbstractPresenter
 
     protected function present(bool $isListing = false): array
     {
+
+        $today = Carbon::now()->locale('en')->englishDayOfWeek;
+        $now = Carbon::now()->format('H:i');
+        
+        $todayHours = $this->shop->shopHours
+            ? $this->shop->shopHours->firstWhere('day', $today)
+            : null;
+
+        $isOpenNow = false;
+
+        if ($todayHours && $todayHours->status == 1) {
+            $opening = Carbon::createFromFormat('H:i', $todayHours->opening_time);
+            $closing = Carbon::createFromFormat('H:i', $todayHours->closing_time);
+
+            $nowTime = Carbon::now();
+            if ($closing->lessThan($opening)) {
+                $closing->addDay();
+            }
+
+            $isOpenNow = $nowTime->between($opening, $closing);
+        }
+
+
         return [
             'id' => $this->shop->id,
             'name' => $this->shop->name,
@@ -31,13 +55,16 @@ class ShopDetailsPresenter extends AbstractPresenter
 
             'description_en' => $this->shop->getTranslation('description', 'en'),
             'description_ar'=> $this->shop->getTranslation('description', 'ar'),
-
+            
+            'time_now'=>   Carbon::now()->format('Y-m-d H:i'),
             'worker_no'=> $this->shop->worker_no,
             'city_id'=> $this->shop->city_id,
             'street'=> $this->shop->street,
             'address_1'=> $this->shop->address_1,
             'address_2'=> $this->shop->address_2,
             'files' => MediaPresenter::collection(mediaItems: $this->shop->getMedia('shops')), //array
+            'is_open_now' => $isOpenNow,
+
             'average_rates' => $this->shop->average_rating,
             'total_rates' => $this->shop->total_rates,
             'is_open' => $this->shop->is_open,
