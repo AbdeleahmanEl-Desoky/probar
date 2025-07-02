8e08814e2b82375c34b31815f3b9e8e6e322bb34
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-07-02 16:51:20 +0300

    update program system

--- modules/Barber/Shop/Filters/ShopFilter.php
+++ modules/Barber/Shop/Filters/ShopFilter.php
@@ -101,7 +101,6 @@ class ShopFilter extends SearchModelFilter
         if ($value !== 'yes') {
             return $this;
         }
-        dd($value);
         return $this->withAvg('rates', 'rate')->orderBy('rates_avg_rate', 'desc');
     }
 }
