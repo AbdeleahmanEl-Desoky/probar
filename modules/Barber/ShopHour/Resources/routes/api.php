f40ad9ffb61728b5714f339ff041bae64d49e9fa
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-08-15 22:12:20 +0300

    add JobOffer task

--- modules/Barber/ShopHour/Resources/routes/api.php
+++ modules/Barber/ShopHour/Resources/routes/api.php
@@ -7,6 +7,6 @@ Route::group(['middleware' => ['auth:api_barbers']], function () {
     Route::get('/', [ShopHourController::class, 'index']);
     Route::post('/', [ShopHourController::class, 'store']);
     Route::get('/{id}', [ShopHourController::class, 'show']);
-    Route::put('/{id}', [ShopHourController::class, 'update']);
+    Route::post('/details', [ShopHourController::class, 'update']);
     Route::delete('/{id}', [ShopHourController::class, 'delete']);
 });
